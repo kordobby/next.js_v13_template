b0822e342ed27110ea896e89229bc1bef90955e8
Yoon Lee <poordobby@naver.com>
2023-04-18 23:12:05 +0900

    add. config files next/ts and prettier

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // pageExtensions: ["ts", "tsx", "md"],
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
