2f7fe093cbf58608e93faa5efed259f4f67d0e4d
Yoon Lee <poordobby@naver.com>
2023-04-18 22:59:36 +0900

    add. dynamic meta tags

--- src/app/dynamic-segment-catch-all-ex/[...slug]/page.tsx
+++ src/app/dynamic-segment-catch-all-ex/[...slug]/page.tsx
@@ -1,6 +1,37 @@
 import Link from "next/link";
+import { Metadata, ResolvingMetadata } from "next";
 
-const Page = ({ params: { slug } }: { params: { slug: string } }) => {
+type PageProps = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+/* ADD DYNAMIC META TAGS AT DYNAMIC ROUTES */
+export const generateMetadata = async (
+  { params, searchParams }: PageProps,
+  parent?: ResolvingMetadata
+): Promise<Metadata> => {
+  // read route params
+  const { slug } = params;
+
+  // fetch data
+  /* EXAMPLE
+  const pokemon = await fetch(`https://.../${id}`).them((res) => res.json());
+   */
+  return {
+    title: slug,
+    openGraph: {
+      title: slug,
+    },
+    twitter: {
+      card: "summary",
+      title: slug,
+    },
+  };
+};
+
+const Page = ({ params, searchParams }: PageProps) => {
+  const { slug } = params;
   return (
     <>
       <p>slug 가 없는건 불가능</p>
